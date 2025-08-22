42eb43e31ec80b9c6ef0162eff2a0afbbeed8b8e
Rithwik Surya <surya.rithwik2005@gmail.com>
2025-08-22 23:15:42 +0530

    Update app.d.ts

--- my-ap/src/app.d.ts
+++ my-ap/src/app.d.ts
@@ -2,11 +2,7 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+
 	}
 }
 
